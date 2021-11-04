7f01e767baf709873f9c3996f1f7a7c076fa73e3
Ahmed-Sherif-ASA <ahmed.sherif@zyda.com>
2021-11-04 03:57:27 +0200

    Added the logic and errors for the max employees number per organization

--- saas_hr/app/models/organization.rb
+++ saas_hr/app/models/organization.rb
@@ -1,4 +1,9 @@
 class Organization < ApplicationRecord
   has_many :companies
   has_many :branches, through: :companies
+  validate :check_max_employees_count
+
+  def check_max_employees_count
+    raise StandardError.new('Employees count exceeded the threshold') if employees_count > max_employees_count
+  end
 end
---
+++ saas_hr/db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20211103200448) do
+
+  create_table "branches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "employees_count", default: 0
+    t.integer  "company_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["company_id"], name: "fk_rails_9d5c1c06e3", using: :btree
+  end
+
+  create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "employees_count", default: 0
+    t.integer  "organization_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["organization_id"], name: "fk_rails_206ba22f2a", using: :btree
+  end
+
+  create_table "employees", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "entity_type"
+    t.integer  "entity_id"
+    t.index ["entity_type", "entity_id"], name: "index_employees_on_entity_type_and_entity_id", using: :btree
+  end
+
+  create_table "organizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "employees_count",     default: 0
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "max_employees_count", default: 0
+  end
+
+  add_foreign_key "branches", "companies"
+  add_foreign_key "companies", "organizations"
+end
